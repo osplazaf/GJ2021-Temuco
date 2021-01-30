058a0d247b279c01267697ffe1f2c9bf637bbcb0
Arturo <40644850+arther-urg@users.noreply.github.com>
2021-01-30 17:50:26 -0300

    scripts
    Para el ingreso de nombre y el sistema de dialogos

---
+++ GJ2021-Temuco/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogo dialogo;
+
+    public void TriggerDialogue ()
+    {
+        FindObjectOfType<DialogueManager>().IniciarDialogo(dialogo);
+    }
+}
